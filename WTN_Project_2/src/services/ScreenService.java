349b80900eef2ccc3da99c9c812284b0056e7a61
ACER <ACER@NIKKI.mshome.net>
2021-06-18 11:29:20 +0530

    WTN_Project2

---
+++ WTN_Project_2/src/services/ScreenService.java
@@ -0,0 +1,29 @@
+package services;
+import screens.FileOptionsScreen;
+import screens.Screen;
+import screens.WelcomeScreen;
+import entities.Directory;
+public class ScreenService {
+	//static Directory dir = new Directory();
+	
+	
+	
+		public static WelcomeScreen WelcomeScreen = new WelcomeScreen();
+	    public static FileOptionsScreen FileOptionsScreen = new FileOptionsScreen();
+	    
+	    
+
+	    public static Screen CurrentScreen = WelcomeScreen;
+
+	    
+	    public static Screen getCurrentScreen() {
+	        return CurrentScreen;
+	    }
+
+	    
+	    public static void setCurrentScreen(Screen currentScreen) {
+	        CurrentScreen = currentScreen;
+	    }
+	    
+
+}
